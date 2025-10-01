93cb975cbcc627aeb7175fb1cec29fb9f8ff6c8d
H Jones <hjones@Hs-MacBook-Pro.local>
2025-10-01 19:27:42 -0400

    starter code

---
+++ src/LetterBox.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class LetterBox extends React.Component {
+  render() {
+    const { letter, isVisible, boxStyle, letterStyle } = this.props;
+
+    const defaultBoxStyle = {
+      border: '1px solid black',
+      width: '50px',
+      height: '50px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '24px',
+      fontWeight: 'bold',
+    };
+
+    const defaultLetterStyle = {
+      visibility: isVisible ? 'visible' : 'hidden',
+      color: 'white', 
+      fontSize: '30px' 
+    };
+
+    const combinedBoxStyle = { ...defaultBoxStyle, ...boxStyle };
+    const combinedLetterStyle = { ...defaultLetterStyle, ...letterStyle };
+
+    return (
+      <div style={combinedBoxStyle}>
+        <span style={combinedLetterStyle}>{letter}</span>
+      </div>
+    );
+  }
+}
+
+export default LetterBox;
